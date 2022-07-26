259f317a17a3259aa7e95aa9c34a15258b02855c
Amy Gori <amydgori@gmail.com>
2022-07-26 17:20:07 -0400

    Add ability to log in with dev server only

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
+import { useState } from 'react'
 import { BookList } from './components/BookList'
 import './App.css'
+import { Login } from './components/Login'
 
 const App = () => {
+  const [token, setToken] = useState(null)
+  const [username, setUsername] = useState('')
+
+  const setAuth = (username, token) => {
+    setToken(token)
+    setUsername(username)
+  }
+
+  const isLoggedIn = username && token
+
+  if (!isLoggedIn) {
+    return <Login setAuth={setAuth} />
+  }
+
   return (
     <>
       <header className="header">
         <h1 className="is-size-1">Books</h1>
       </header>
-      <BookList />
+      <div className="logged-in-message">
+        Hello, you're logged in as {username}
+      </div>
+      <BookList token={token} />
     </>
   )
 }
---
+++ src/components/Login.js
@@ -0,0 +1,46 @@
+import axios from 'axios'
+import { useState } from 'react'
+export const Login = ({ setAuth }) => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    // when the form submits, make an ajax request to the login endpoint
+    // capture the auth token in state
+    axios
+      .post('http://127.0.0.1:8000/api/auth/token/login/', {
+        username: username,
+        password: password,
+      })
+      .then((res) => {
+        setAuth(username, password)
+      })
+  }
+  return (
+    <>
+      <h2>Hello I am the Login Form</h2>
+      <form id="login-form" onSubmit={handleSubmit}>
+        <div className="form-controls">
+          <label htmlFor="username-field">username</label>
+          <input
+            id="username-field"
+            onChange={(e) => setUsername(e.target.value)}
+            type="text"
+          />
+        </div>
+        <div className="form-controls">
+          <label htmlFor="password-field">password</label>
+          <input
+            id="password-field"
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+          />
+        </div>
+        <div className="form-submit">
+          <input type="submit" value="Log In" />
+        </div>
+      </form>
+    </>
+  )
+}
